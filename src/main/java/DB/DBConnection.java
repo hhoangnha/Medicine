a8fa806195900f0f19c2503ccfef24bbafbe4079
ThaiVinh <hathaivinhs@gmail.com>
2024-02-28 13:15:15 +0700

    manage manufacturer module

--- src/main/java/DB/DBConnection.java
+++ src/main/java/DB/DBConnection.java
@@ -14,24 +14,30 @@ import java.sql.SQLException;
  * @author Nguyen Hoang Nha - CE170092
  */
 public class DBConnection {
-    public static Connection connect() throws ClassNotFoundException, SQLException{
-        Class.forName("com.mysql.cj.jdbc.Driver");
-//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-//            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:8889/Medicine?user=root&password=root");
-////            Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=Medicine;user=sa;password=12345;encrypt=true;trustServerCertificate=true");
-//        return conn;
-        String host = "jdbc:mysql://localhost:8889/Medicine";
-        String username = "root";
-        String password = "root";
-        try {
-            Connection conn = DriverManager.getConnection(host, username, password);
-//            System.out.println("Connected to MySQL database");
-            return conn;
-        } catch (SQLException e) {
-            System.out.println("Failed to connect to MySQL database");
-            e.printStackTrace();
-        }
-        return null;
-        
+//    public static Connection connect() throws ClassNotFoundException, SQLException{
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+////            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+////            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:8889/Medicine?user=root&password=root");
+//////            Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=Medicine;user=sa;password=12345;encrypt=true;trustServerCertificate=true");
+////        return conn;
+//        String host = "jdbc:mysql://localhost:8889/Medicine";
+//        String username = "root";
+//        String password = "root";
+//        try {
+//            Connection conn = DriverManager.getConnection(host, username, password);
+////            System.out.println("Connected to MySQL database");
+//            return conn;
+//        } catch (SQLException e) {
+//            System.out.println("Failed to connect to MySQL database");
+//            e.printStackTrace();
+//        }
+//        return null;
+//        
+//    }
+    
+     public static Connection connect() throws ClassNotFoundException, SQLException{
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=Medicine;user=sa;password=12345;encrypt=true;trustServerCertificate=true");
+        return conn;
     }
 }
