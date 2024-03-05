4ec1003d57cf53cfbc4168184c4c0fdc8b76604c
tranhoangquy98917 <160830020+tranhoangquy98917@users.noreply.github.com>
2024-03-05 12:44:52 +0700

    Add files via upload

---
+++ src/main/java/Controllers/OrderController.java
@@ -0,0 +1,114 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controllers;
+
+import Daos.OrderDAO;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+/**
+ *
+ * @author User
+ */
+public class OrderController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet OrderController</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet OrderController at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String path = request.getRequestURI();
+        HttpSession session = (HttpSession) request.getSession();
+        if (path.endsWith("/OrderController")) {
+            request.getRequestDispatcher("admin-order-list.jsp").forward(request, response);
+        }
+        if (path.startsWith("/OrderController/deleteUnit")) {
+                String[] s = path.split("/");
+                try {
+                    int OrderID = Integer.parseInt(s[s.length - 1]);
+                    OrderDAO o = new OrderDAO();
+//                    ct.delete(cateid, 0);
+                    o.delete(OrderID);
+                    session.setAttribute("msgSuccess", "Delete successfully!");
+                    response.sendRedirect("/OrderController");
+                } catch (Exception ex) {
+                    response.sendRedirect("/OrderController");
+                }
+
+            }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        OrderDAO udao = new OrderDAO();
+        if (request.getParameter("btnDeleteOrder") != null) {
+            int OrderID = Integer.parseInt(request.getParameter("OrderID"));
+            try {
+                udao.delete(OrderID);
+                response.sendRedirect("/OrderController");
+            } catch (Exception e) {
+                response.sendRedirect("/UnitController/deleteOrder/" + OrderID);
+            }
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
